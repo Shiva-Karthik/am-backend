623a57c2a0d0e81e74f6c6e46d9a4a97ad7a04a7
Shiva-Karthik <karthikkonathala6@gmail.com>
2022-04-17 19:13:22 +0530

    added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,14 @@
 const express = require("express");
-const userSchema = require("../models/user.model");
+const User = require("../models/user.model");
 
 const router = express.Router();
+router.get('/list', async (req, res) => {
+    try {
+        const user = await User.find().lean().exec();
+        return res.status(200).send(user)
+    }
+    catch(err){
+        return res.status(400).send({message : "users not found"})
+    }
+})
 module.exports = router;
