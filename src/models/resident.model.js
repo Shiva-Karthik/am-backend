2b29cc1d20182b6c7be28a278335a9b6eed6df0e
Shiva-Karthik <karthikkonathala6@gmail.com>
2022-04-17 20:06:14 +0530

    added

--- src/models/resident.model.js
+++ src/models/resident.model.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
-
-const residentSchema = new mongoose.Schema({
-        userId: { type: mongoose.Schema.Types.ObjectId, ref:"user",required:true}, 
-        gender : {type : String, default : "male", reqrired : true},
-        age : {type : Number, default : 18, required : true},
-        flatId : {type : mongoose.Schema.Types.ObjectId, ref : "flat", required : true},
+const residentSchema = new mongoose.Schema(
+  {
+    name: { type: String, reqrired: true },
+    gender: { type: String, default: "male", reqrired: true },
+    age: { type: Number, default: 18, required: true },
+    flatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "flat",
+      required: true,
     },
-    {
+  },
+  {
     versionKey: false,
-    }
+  }
 );
 
-module.exports =  mongoose.model("resident", residentSchema);
+module.exports = mongoose.model("resident", residentSchema);
