82d8c7c8c8365fbf43d4a27e0f1a557c21d26962
Shiva-Karthik <karthikkonathala6@gmail.com>
2022-04-20 23:44:25 +0530

    pagination

--- src/controllers/flat.controller.js
+++ src/controllers/flat.controller.js
@@ -28,23 +28,23 @@ router.get("/list", async (req, res) => {
     return res.status(400).send(err.message);
   }
 });
-router.get('/list', async (req, res) => {
-  try {
-    const page = req.query.page || 1;
-    const size = req.query.size || 15;
-    const flat = await Flat.find().skip((page - 1) * size)
-    .limit(size)
-    .lean()
-    .exec();
-    const totalPages = Math.ceil(
-      (await Flat.find().countDocuments()) / size
-    );
-    return res.status(200).send({flat, totalPages})
-  }
-  catch(err) {
-    return res.status(400).send(err.message);
-  }
-})
+// router.get('/list', async (req, res) => {
+//   try {
+//     const page = req.query.page || 1;
+//     const size = req.query.size || 15;
+//     const flat = await Flat.find().skip((page - 1) * size)
+//     .limit(size)
+//     .lean()
+//     .exec();
+//     const totalPages = Math.ceil(
+//       (await Flat.find().countDocuments()) / size
+//     );
+//     return res.status(200).send({flat, totalPages})
+//   }
+//   catch(err) {
+//     return res.status(400).send(err.message);
+//   }
+// })
 router.get("/:id", async (req, res) => {
   try {
     const flatList = await Flat.findById(req.params.id).lean().exec();
