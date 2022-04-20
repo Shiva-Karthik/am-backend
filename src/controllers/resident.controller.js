943a170f6ec29407209dc809c2e87e831458310c
Shiva-Karthik <karthikkonathala6@gmail.com>
2022-04-20 22:24:35 +0530

    pagination

--- src/controllers/resident.controller.js
+++ src/controllers/resident.controller.js
@@ -11,32 +11,40 @@ router.post("/post", async (req, res) => {
     return res.status(400).send(err.message);
   }
 });
-router.get('/list', async (req, res) => {
+router.get("/list", async (req, res) => {
   try {
-    const residentData = await Resident.find().populate([{path : "flatId"}]).lean().exec();
+    const residentData = await Resident.find()
+      .populate([{ path: "flatId" }])
+      .lean()
+      .exec();
     return res.status(200).send(residentData);
-  }
-  catch(err) {
+  } catch (err) {
     return res.status(400).send(err.message);
-    
   }
-})
+});
 router.get("/:id", async (req, res) => {
   try {
     const flatID = req.params.id;
-    const resident = await Resident.find({flatId : flatID}).select({flatId : 0}).lean().exec();
+    const resident = await Resident.find({ flatId: flatID })
+      .select({ flatId: 0 })
+      .lean()
+      .exec();
     return res.status(200).send(resident);
   } catch (err) {
     return res.status(400).send(err.message);
   }
 });
-router.patch('/:id', async (req, res) => {
+
+router.patch("/:id", async (req, res) => {
   try {
-    const resident = await Resident.findByIdAndUpdate(req.params.id, req.body, {new : true}).lean().exec();
+    const resident = await Resident.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+      .lean()
+      .exec();
     return res.status(200).send(resident);
+  } catch (err) {
+    return res.status(400).send(err.message);
   }
-  catch(err) {
-    return res.status(400).send(err.message)
-  }
-})
+});
 module.exports = router;
